044d43cbf7d80c6bc13b00c493acb03cecc3482b
joseleo <joseleon.engineer@gmail.com>
2015-07-18 10:37:00 +0200

    Modificación de items.py, __init__.py

--- OSLcrawler/OSLcrawler/items.py
+++ OSLcrawler/OSLcrawler/items.py
@@ -10,5 +10,7 @@ import scrapy
 
 class OslcrawlerItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    author = scrapy.Field()
+    content = scrapy.Field()
     pass
--- OSLcrawler/OSLcrawler/spiders/__init__.py
+++ OSLcrawler/OSLcrawler/spiders/__init__.py
@@ -2,3 +2,42 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from OSLcrawler.items import OslcrawlerItem
+
+class oslSpider(CrawlSpider):
+    name = 'OSL'
+    allowed_domains = ['osl.ugr.es']
+    start_urls = ['http://osl.ugr.es']
+
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=(
+                    '/page/\d+/',
+                 ),
+            ),
+        ),
+
+        Rule(
+            LinkExtractor(
+                allow=(
+                    '/\d+/\d+/\d+/',
+                ),
+            ),
+        ),
+        callback = 'parse_item',
+        follow = True
+        ),
+    )
+
+    def parse_item(self, response):
+    i = OslcrawlerItem()
+    i['title'] = response.xpath('//h1[@class="entry-title format icon"]/text()').extract()
+    i['author'] = response.xpath('//span[@class="author vcard"]/a[@rel="author"]/text()').extract()
+    i['content'] = response.xpath('/html/section[@class="entry-content"]//p/text()').extract()
+    return i 
